1e0cc4a3f8142b4ac74fc4de8d89006de108bb81
Ronald Blanco <pnald.blanco@gmail.com>
2017-09-27 18:05:46 +0000

    Server.js clean

--- server.js
+++ server.js
@@ -21,7 +21,6 @@ mongoose.connect(process.env.MONGO_URI);
 mongoose.Promise = global.Promise;
 
 /////EMAIL CONFIG////////////////////////////////////////////////////////////////////////////
-//app.use('/emailjs', express.static(process.cwd() + '/node_modules/emailjs'));
 var emailServer = {
     'user' : process.env.EMAILUSER,
     'password' : process.env.EMAILPASS,
@@ -29,25 +28,6 @@ var emailServer = {
     'port' : process.env.EMAILPORT
 };
 ////////////////////////////////////////////////////////////////////////////////////
-
-/*app.use('/controllers', express.static(process.cwd() + '/app/controllers'));
-app.use('/public', express.static(process.cwd() + '/public'));
-app.use('/dist', express.static(process.cwd() + '/dist'));
-app.use('/common', express.static(process.cwd() + '/app/common'));
-
-//////////////////////////////////////////////
-app.use(require('cookie-parser')());
-app.use(require('body-parser').urlencoded({ extended: true }));
-//////////////////////////////////////////////
-
-app.use(session({
-	secret: 'secretClementine',
-	resave: false,
-	saveUninitialized: true
-}));
-
-app.use(passport.initialize());
-app.use(passport.session());*/
 
 if (process.env.NODE_ENV === 'development'){
     //Grunt build execution
@@ -70,35 +50,7 @@ if (process.env.NODE_ENV === 'development'){
     /////////////////////////////////////////////////
 }
 
-//Forzing Cache of static/////////////////////////
-/*app.use(functions.cacheIt);
-/////////////////////////////////////////////////
-
-//COMPRESSION////////////////////////////////////
-app.use(compression({filter: functions.shouldCompress}));*/
-/////////////////////////////////////////////////
-
 routes(app, passport, passportTwitter, passportLocal, emailServer);
-
-//Uncomment to used the Websocket Controller
-//using: socket.io http and model config.js as test
-//WEBSOCKET///////////////////////////
-/*var server = require('http').createServer(app);
-var io = require('socket.io')(server);
-var webSocketHandler = require(process.cwd() + '/app/controllers/webSocketHandler.server.js');
-
-var numClients = 0;
-var endpoint = io
-  .of('/')
-  .on('connection', function (socket) {
-      webSocketHandler.respond(endpoint,socket);
-  });
-  
-  var port = process.env.PORT || 8080;
-  server.listen(port,  function () {
-	console.log('Node.js with WebSocket listening on port ' + port + '...');
-});*/
-//WEBSOCKET//////////////////////////
 
 var port = process.env.PORT || 8080;
 app.listen(port,  function () {
