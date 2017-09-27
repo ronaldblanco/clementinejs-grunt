d4eeda68bcc4c9bf76b97b9cb2a2d85e665a835f
Ronald Blanco <pnald.blanco@gmail.com>
2017-09-27 18:49:43 +0000

    Client folder with controllers

--- app/grunt/concat.js
+++ app/grunt/concat.js
@@ -7,15 +7,15 @@ module.exports = {
         dest: 'dist/js/<%= pkg.name %>.js'
       },*/
       profile: {
-        src: ['app/common/ajax-functions.js','app/controllers/profileController.client.js'],
+        src: ['app/common/ajax-functions.js','app/controllers/client/profileController.client.js'],
         dest: 'dist/js/profile.<%= pkg.name %>.js'
       },
       userlocal: {
-        src: ['app/common/ajax-functions.js','app/controllers/userController.client.js'],
+        src: ['app/common/ajax-functions.js','app/controllers/client/userController.client.js'],
         dest: 'dist/js/user.<%= pkg.name %>.js'
       },
       index: {
-        src: ['app/common/ajax-functions.js','app/controllers/clickController.client.js','app/controllers/dataController.client.js','app/controllers/profileController.client.js','app/controllers/client/webSocketController.client.js'],
+        src: ['app/common/ajax-functions.js','app/controllers/client/clickController.client.js','app/controllers/client/dataController.client.js','app/controllers/client/profileController.client.js','app/controllers/client/webSocketController.client.js'],
         dest: 'dist/js/index.<%= pkg.name %>.js'
       }
     };
